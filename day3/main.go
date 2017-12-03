ec92c7252536ed5ba34c8142a1d9feba3dcde1cf
Augusto Roman <aroman@gmail.com>
2017-12-03 01:04:55 -0700

    some docs for day3

--- day3/main.go
+++ day3/main.go
@@ -6,6 +6,25 @@ import (
 	"math"
 )
 
+// 37  36  35  34  33  32  31
+// 38  17  16  15  14  13  30
+// 39  18   5   4   3  12  29
+// 40  19   6   1   2  11  28
+// 41  20   7   8   9  10  27
+// 42  21  22  23  24  25  26
+// 43  44  45  46  47  48  49
+
+// Ring | Start # | End #
+//  1   |   1     |   1  = 1^2
+//  3   |   2     |   9  = 3^2
+//  5   |  10     |   25 = 5^2
+//  7   |  26     |   49 = 7^2
+
+// Data from square 1 is carried 0 steps, since it's at the access port.
+// Data from square 12 is carried 3 steps, such as: down, left, left.
+// Data from square 23 is carried only 2 steps: up twice.
+// Data from square 1024 must be carried 31 steps.
+
 func ring(n int) int {
 	r0 := int(math.Ceil(math.Sqrt(float64(n))))
 	if r0%2 == 0 {
